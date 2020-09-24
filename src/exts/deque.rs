b3f3422df910f23119143e29f7bf1b44daf152eb
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-09-24 12:46:09 +0200

    Implement {En,De}code for VecDeque

--- src/exts/deque.rs
+++ src/exts/deque.rs
@@ -10,24 +10,65 @@
  * │                                          Imports                                           │ *
 \* └────────────────────────────────────────────────────────────────────────────────────────────┘ */
 
-use cfg_if::cfg_if;
+use crate::{Decode, Encode};
+use std::collections::VecDeque;
+use std::io::{self, Read, Write};
 
 /* ┌────────────────────────────────────────────────────────────────────────────────────────────┐ *\
- * │                                       cfg_if! { .. }                                       │ *
+ * │                              impl {En,De}code for VecDeque<T>                              │ *
 \* └────────────────────────────────────────────────────────────────────────────────────────────┘ */
 
-cfg_if! {
-    if #[cfg(feature = "collections")] {
-        mod deque;
-        mod list;
-        mod map;
-        mod set;
-        mod vec;
+impl<T: Encode> Encode for VecDeque<T>
+where
+    T::Error: From<io::Error>,
+{
+    type Error = T::Error;
+
+    fn size(&self) -> Result<usize, Self::Error> {
+        if self.len() > u16::MAX as usize {
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "buf.len() > u16::MAX").into());
+        }
+
+        let mut size = (self.len() as u16).fast_size();
+        for elem in self {
+            size += elem.size()?;
+        }
+
+        Ok(size)
+    }
+
+    fn fast_size(&self) -> usize {
+        (self.len() as u16).fast_size() + self.get(0).map(|elem| elem.fast_size() * self.len()).unwrap_or(0)
+    }
+
+    fn encode_into<W: Write>(&self, mut writer: W) -> Result<(), Self::Error> {
+        if self.len() > u16::MAX as usize {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "buf.len() > u16::MAX").into())
+        } else {
+            (self.len() as u16).encode_into(&mut writer)?;
+            for elem in self {
+                elem.encode_into(&mut writer)?;
+            }
+
+            Ok(())
+        }
     }
 }
 
-cfg_if! {
-    if #[cfg(feature = "net")] {
-        mod net;
+impl<T: Decode> Decode for VecDeque<T>
+where
+    <T as Encode>::Error: From<io::Error>,
+{
+    fn decode_with_len_from<R: Read>(mut reader: R) -> Result<(Self, usize), Self::Error> {
+        let (len, mut read) = u16::decode_with_len_from(&mut reader)?;
+        let mut elems = VecDeque::with_capacity(len as usize);
+
+        for _ in 0..len {
+            let (elem, readb) = T::decode_with_len_from(&mut reader)?;
+            elems.push_back(elem);
+            read += readb;
+        }
+
+        Ok((elems, read))
     }
 }
