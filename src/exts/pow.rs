9e4dd081931de5fbc29b281fb481143f17c073e6
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-09-24 14:01:12 +0200

    Add 'pow' feature

--- src/exts/pow.rs
+++ src/exts/pow.rs
@@ -10,43 +10,44 @@
  * │                                          Imports                                           │ *
 \* └────────────────────────────────────────────────────────────────────────────────────────────┘ */
 
-use cfg_if::cfg_if;
+use crate::{Decode, Encode};
+use pow::Proofs;
+use std::collections::BTreeMap;
+use std::io::{self, Read, Write};
 
 /* ┌────────────────────────────────────────────────────────────────────────────────────────────┐ *\
- * │                                       cfg_if! { .. }                                       │ *
+ * │                                impl {En,De}code for Proofs                                 │ *
 \* └────────────────────────────────────────────────────────────────────────────────────────────┘ */
 
-cfg_if! {
-    if #[cfg(feature = "chrono")] {
-        mod chrono;
+impl Encode for Proofs {
+    type Error = io::Error;
+
+    fn fast_size(&self) -> usize {
+        self.description().fast_size()
+            + (self.levels() as u16).fast_size()
+            + (self.proofs() as u16).fast_size()
+            + self.as_nodes().fast_size()
+    }
+
+    fn encode_into<W: Write>(&self, mut writer: W) -> Result<(), Self::Error> {
+        if self.levels() > u16::MAX as usize || self.proofs() > u16::MAX as usize {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "levels and proofs should be < u16::MAX"))
+        } else {
+            self.description().encode_into(&mut writer)?;
+            (self.levels() as u16).encode_into(&mut writer)?;
+            (self.proofs() as u16).encode_into(&mut writer)?;
+            self.as_nodes().encode_into(&mut writer)
+        }
     }
 }
 
-cfg_if! {
-    if #[cfg(feature = "collections")] {
-        mod deque;
-        mod heap;
-        mod list;
-        mod map;
-        mod set;
-        mod vec;
-    }
-}
+impl Decode for Proofs {
+    fn decode_with_len_from<R: Read>(mut reader: R) -> Result<(Self, usize), Self::Error> {
+        let (desc, read1) = Vec::<u8>::decode_with_len_from(&mut reader)?;
+        let (levels, read2) = usize::decode_with_len_from(&mut reader)?;
+        let (proofs, read3) = usize::decode_with_len_from(&mut reader)?;
+        let (nodes, read4) = BTreeMap::<usize, [u8; 32]>::decode_with_len_from(&mut reader)?;
 
-cfg_if! {
-    if #[cfg(feature = "ed25519")] {
-        mod ed25519;
-    }
-}
-
-cfg_if! {
-    if #[cfg(feature = "net")] {
-        mod net;
-    }
-}
-
-cfg_if! {
-    if #[cfg(feature = "pow")] {
-        mod pow;
+        Ok((Proofs::new(desc, levels as usize, proofs as usize, nodes), read1 + read2 + read3 + read4))
     }
 }
